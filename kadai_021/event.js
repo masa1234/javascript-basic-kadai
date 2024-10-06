2526a089edff253f9001d4685e155a97b9526c32
masa1234 <17587674+masa1234@users.noreply.github.com>
2024-10-06 14:50:29 +0900

    修正

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,5 +1,8 @@
 const btn = document.getElementById("btn");
+const h2Text = document.getElementById("text");
 btn.addEventListener("click", () => {
-  const h2Text = document.getElementById("text");
-  h2Text.textContent = "ボタンをクリックしました";
+  setTimeout(() => {
+    console.log("2秒経過しました");
+    h2Text.textContent = "ボタンをクリックしました";
+  }, 2000);
 });
